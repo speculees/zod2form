0edecd84cf0734cb006c0ae76179fa4fb199f1ab
Speculees <speculees@gmail.com>
2024-05-03 23:11:53 +0200

    code format (#11)
    * chore: format code (prettier)
    
    * chore: strict identation
    
    * chore: format package.json (eslint)
    
    * chore(zod2form): Ignore node_modules in ESLint ignore file.
    
    * chore: Update .gitignore to ignore all node_modules and dist folders.
    
    ---------
    
    Co-authored-by: Petar Markov <petar.markov@vegait.rs>

--- lib/zod2form/src/lib/derive-default-values.spec.ts
+++ lib/zod2form/src/lib/derive-default-values.spec.ts
@@ -3,56 +3,56 @@ import { location, user } from './derive-inputs.spec';
 import deriveDefaultValues from './derive-default-values';
 
 describe('deriveDefaultValues', () => {
-    it('should derive default values for user', () => {
-        const schema = z.object(user);
-        const defaultValues = deriveDefaultValues(schema);
-        expect(defaultValues).toEqual({ 
-            "age": 25,
-            "email": "johndoe@me.com",
-            "firstName": "John",
-            "id": "123e4567-e89b-12d3-a456-426614174000",
-            "isActive": true,
-            "lastName": "Doe",
-            "picture": "https://i.pravatar.cc/300",
-            "type": "USER",
-            "userName": "johndoe",
-        });
+  it('should derive default values for user', () => {
+    const schema = z.object(user);
+    const defaultValues = deriveDefaultValues(schema);
+    expect(defaultValues).toEqual({
+      age: 25,
+      email: 'johndoe@me.com',
+      firstName: 'John',
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      isActive: true,
+      lastName: 'Doe',
+      picture: 'https://i.pravatar.cc/300',
+      type: 'USER',
+      userName: 'johndoe',
     });
+  });
 
-    it('should derive default values for location', () => {
-        const schema = z.object(location);
-        const defaultValues = deriveDefaultValues(schema);
-        expect(defaultValues).toEqual({ 
-            "address": '',
-            "city": '',
-            "country": '',
-            "state": '',
-            "type": "LOCATION",
-            "zipcode": ''
-        });
+  it('should derive default values for location', () => {
+    const schema = z.object(location);
+    const defaultValues = deriveDefaultValues(schema);
+    expect(defaultValues).toEqual({
+      address: '',
+      city: '',
+      country: '',
+      state: '',
+      type: 'LOCATION',
+      zipcode: '',
     });
+  });
 
-    it('should derive default values for user and location', () => {
-        const schema = z.object({ ...user, location: z.object(location) });
-        const defaultValues = deriveDefaultValues(schema);
-        expect(defaultValues).toEqual({
-            "age": 25,
-            "email": "johndoe@me.com",
-            "firstName": "John",
-            "id": "123e4567-e89b-12d3-a456-426614174000",
-            "isActive": true,
-            "lastName": "Doe",
-            "picture": "https://i.pravatar.cc/300",
-            "userName": "johndoe",
-            "type": "USER",
-            "location": {
-                "address": '',
-                "city": '',
-                "country": '',
-                "state": '',
-                "type": "LOCATION",
-                "zipcode": '',
-            },
-        });
-    })
-})
+  it('should derive default values for user and location', () => {
+    const schema = z.object({ ...user, location: z.object(location) });
+    const defaultValues = deriveDefaultValues(schema);
+    expect(defaultValues).toEqual({
+      age: 25,
+      email: 'johndoe@me.com',
+      firstName: 'John',
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      isActive: true,
+      lastName: 'Doe',
+      picture: 'https://i.pravatar.cc/300',
+      userName: 'johndoe',
+      type: 'USER',
+      location: {
+        address: '',
+        city: '',
+        country: '',
+        state: '',
+        type: 'LOCATION',
+        zipcode: '',
+      },
+    });
+  });
+});
--- lib/zod2form/src/lib/derive-default-values.ts
+++ lib/zod2form/src/lib/derive-default-values.ts
@@ -3,40 +3,42 @@ import * as Utils from './utils';
 
 type ObjectRawShape = z.ZodObject<z.ZodRawShape>;
 
-function deriveDefaultValues<S extends ObjectRawShape = ObjectRawShape>(schema: S): z.infer<S> {
-    let defaultValue: unknown;
-    const defaultValues: Record<string, unknown> = {};
-    const keys = Object.keys(schema.shape);
+function deriveDefaultValues<S extends ObjectRawShape = ObjectRawShape>(
+  schema: S
+): z.infer<S> {
+  let defaultValue: unknown;
+  const defaultValues: Record<string, unknown> = {};
+  const keys = Object.keys(schema.shape);
 
-    for (const key of keys) {
-        const element = Utils.peel(schema.shape[key], (el) => {
-            if (Utils.isZodDefault(el)) {
-                defaultValue = el._def.defaultValue();
-            }
-        }) as z.ZodNumber;        
+  for (const key of keys) {
+    const element = Utils.peel(schema.shape[key], (el) => {
+      if (Utils.isZodDefault(el)) {
+        defaultValue = el._def.defaultValue();
+      }
+    }) as z.ZodNumber;
 
-        if (Utils.isZodNumber(element)) {
-            defaultValues[key] = defaultValue ?? 0;
-        }
-
-        if (Utils.isZodString(element)) {
-            defaultValues[key] = defaultValue ?? '';
-        }
-
-        if (Utils.isZodBoolean(element)) {
-            defaultValues[key] = defaultValue ?? false;
-        }
-
-        if (Utils.isZodObject(element)) {
-            defaultValues[key] = deriveDefaultValues(element);
-        }
-
-        if (Utils.isZodLiteral(element)) {
-            defaultValues[key] = element.value;
-        }
+    if (Utils.isZodNumber(element)) {
+      defaultValues[key] = defaultValue ?? 0;
     }
 
-    return defaultValues;
+    if (Utils.isZodString(element)) {
+      defaultValues[key] = defaultValue ?? '';
+    }
+
+    if (Utils.isZodBoolean(element)) {
+      defaultValues[key] = defaultValue ?? false;
+    }
+
+    if (Utils.isZodObject(element)) {
+      defaultValues[key] = deriveDefaultValues(element);
+    }
+
+    if (Utils.isZodLiteral(element)) {
+      defaultValues[key] = element.value;
+    }
+  }
+
+  return defaultValues;
 }
 
 export default deriveDefaultValues;
