690ebcc37d4bb394acc1296c52f8d5847c9e7daf
Speculees <speculees@gmail.com>
2024-05-04 02:09:06 +0200

    feat(react-demo): welcome page

---
+++ apps/react-demo/src/app/examples.tsx
@@ -0,0 +1,10 @@
+const Examples: React.FC = () => {
+  return (
+    <>
+      <h1>Examples</h1>
+      <p>TODO</p>   
+    </>
+  );
+}
+
+export default Examples;
---
+++ apps/react-demo/src/app/getting-started.tsx
@@ -0,0 +1,10 @@
+const GettingStarted: React.FC = () => {
+  return (
+    <>
+      <h1>Getting Started</h1>
+      <p>TODO</p>   
+    </>
+  );
+}
+
+export default GettingStarted;
